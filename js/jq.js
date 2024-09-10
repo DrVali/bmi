813db65ce6178d1f6cf9a34600af3730e312a4ab
DrVali <ahmadi.world@gmail.com>
2024-09-10 10:21:25 +0330

    Create jq.js

---
+++ js/jq.js
@@ -0,0 +1,35 @@
+  function bmicalc() {
+	  
+		  let weight_parametr = document.getElementById("weight").value;
+		  let height_parametr = document.getElementById("height").value;  
+		  height_parametr /= 100;
+		  let bmi_text = document.getElementById("bmi-text");
+		  let bmi = weight_parametr / (height_parametr * height_parametr) ;
+		
+		 if(bmi < 18.5 ){
+			 bmi_text.innerHTML="Underweight";
+		 }
+		 else if(18.5 <= bmi && bmi < 25 ){
+			  
+			 bmi_text.innerHTML="Normal weight";
+				 
+		 }
+		 
+		 else if(25 <= bmi && bmi < 30 ){
+			  
+			 bmi_text.innerHTML="Overweight";
+				 
+		 }
+     else if( bmi >= 30){
+		  bmi_text.innerHTML="Obese";
+				 
+		 }
+    }
+
+
+	function Reset(){
+			 document.getElementById("weight").value= "";
+			 document.getElementById("height").innerHTML= "";
+			  document.getElementById("bmi-text").innerHTML= "";
+			 
+	}
